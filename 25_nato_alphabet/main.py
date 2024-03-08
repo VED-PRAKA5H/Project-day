92418870459932baf87a422872b282c5c13c7bb7
VED-PRAKA5H <vihastvideos21@gmail.com>
2024-03-08 12:00:00 +0530

    error handling

--- 25_nato_alphabet/main.py
+++ 25_nato_alphabet/main.py
@@ -6,7 +6,13 @@ df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
 # TODO 2: take input from user then create a list
-user_word = list(input("Enter a word to find NATO phonetic:").upper())
+while True:
+    user_word = list(input("Enter a word to find NATO phonetic: ").upper())
+    try:
+        answer_list = [nato_alphabet_dict[letter] for letter in user_word]
+        print(answer_list)
+        break
+    except KeyError:
+        print("Sorry only letter in the alphabet please.")
 
-answer_list = [nato_alphabet_dict[letter] for letter in user_word]
-print(answer_list)
+
