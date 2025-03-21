8a26a94c45dac0274f48001aa1efb49c65c6dca1
VED-PRAKA5H <vihastvideos21@gmail.com>
2025-03-21 14:21:53 +0530

    updated main.py

--- 22_mail_merge/main.py
+++ 22_mail_merge/main.py
@@ -1,3 +1,9 @@
+import os
+
+directory = './Output/ReadyToSend'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
 with open('./Input/Letters/starting_letter.txt') as file:
     letter = file.read()
 with open('./Input/Names/invited_names.txt') as f:
@@ -5,30 +11,27 @@ with open('./Input/Names/invited_names.txt') as f:
     y = x.split('\n')
     for name in y:
         with open('./Input/Letters/starting_letter.txt') as file:
-            file_path = f'./Output/ReadyToSend/letter_for_{name}.txt'
+            file_path = f'{directory}/letter_for_{name}.txt'
             new_letter = letter.replace('[name]', name)
-            with open(file_path, 'w') as file1:
+            with open(file_path, "w") as file1:
                 file1.write(new_letter)
 
-            with open(file_path) as t:
-                print(t.read())
 
 
-
-                                    ##################################### METHOD 2#########################################
-
-PLACEHOLDER = "[name]"
-
-
-with open("./Input/Names/invited_names.txt") as names_file:
-    names = names_file.readlines()
-
-with open("./Input/Letters/starting_letter.txt") as letter_file:
-    letter_contents = letter_file.read()
-    for name in names:
-        stripped_name = name.strip()
-        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
-        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
-            completed_letter.write(new_letter)
+                    ##################################### METHOD 2#########################################
+#
+# PLACEHOLDER = "[name]"
+#
+#
+# with open("./Input/Names/invited_names.txt") as names_file:
+#     names = names_file.readlines()
+#
+# with open("./Input/Letters/starting_letter.txt") as letter_file:
+#     letter_contents = letter_file.read()
+#     for name in names:
+#         stripped_name = name.strip()
+#         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+#         with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+#             completed_letter.write(new_letter)
 
   
